e75e239c893be5933028383656dd6e63600bf7e9
linconviana <linconviana@gmail.com>
2022-01-11 00:00:15 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div >
-      <h1>Pagina Principal</h1>
-    </div>
+ 
+    <Navbar />
+
   );
 }
 
